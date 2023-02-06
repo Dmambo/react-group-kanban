1a3984fe6a18b83ac47e36783541aefbf9e9ab99
Yash <74345713+yash244466666@users.noreply.github.com>
2023-02-06 19:42:44 +0600

    Add Myprofile page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Header from './components/Header/Header';
+import MyProfile from './components/MyProfile/MyProfile';
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path="/profile" element={<MyProfile />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/MyProfile/MyProfile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <div>
+    <h1>My Profile</h1>
+  </div>
+);
+export default MyProfile;
