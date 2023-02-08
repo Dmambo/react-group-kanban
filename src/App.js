9492beba4831480b067a5236c3433667b7bf0718
Dmambo <mambotrader0@gmail.com>
2023-02-08 21:56:08 +0530

    Fixed reservation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import MyProfile from './components/MyProfile/MyProfile';
 import Rockets from './pages/Rockect';
 import Missions from './components/Missions/Missions';
+import { fetchRockets } from './redux/rocket';
 
 import Header from './components/Header/Header';
 
-const App = () => (
-  <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="/missions" element={<Missions />} />
-        <Route path="/profile" element={<MyProfile />} />
-      </Routes>
-    </Router>
-  </>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets({ method: 'GET' }));
+  }, [dispatch]);
+
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<MyProfile />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 export default App;
--- src/redux/rocket.js
+++ src/redux/rocket.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const url = 'https://api.spacexdata.com/v4/rockets';
-const FETCH_ROCKETS = 'spaceX/rockets/FETCH_ROCKETS';
-const LIST_ROCKETS = 'spaceX/rockets/LIST_ROCKETS';
+const ROCKETS = 'spaceX/rockets/ROCKETS';
+const ROCKET = 'spaceX/rockets/ROCKET';
 const RESERVE_ROCKET = 'spaceX/rockets/RESERVE_ROCKET';
 
-export const list = (rockets) => ({ type: LIST_ROCKETS, rockets });
+export const list = (rockets) => ({ type: ROCKET, rockets });
 export const reserve = (id) => ({ type: RESERVE_ROCKET, id });
 
 const get = () => fetch(url)
@@ -24,7 +24,7 @@ const get = () => fetch(url)
   }));
 
 export const fetchRockets = createAsyncThunk(
-  FETCH_ROCKETS,
+  ROCKETS,
   async (args, thunkAPI) => {
     try {
       let rockets;
@@ -44,7 +44,7 @@ export const fetchRockets = createAsyncThunk(
 
 export default (state = [], action) => {
   switch (action.type) {
-    case LIST_ROCKETS:
+    case ROCKET:
       return action.rockets;
     case RESERVE_ROCKET:
       return state.map((rocket) => (rocket.id === action.id
